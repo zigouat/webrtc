989519dec42426d8bf93eb5eab4519a6b8150d92
Billal GHILAS <bilal@evercam.io>
2026-08-30 14:38:57 +0100

    data channels offer/answer

--- src/sctp_transport.zig
+++ src/sctp_transport.zig
@@ -7,12 +7,11 @@ const SctpTranport = @This();
 
 const Logger = std.log.scoped(.sctp_transport);
 
-pub const ConnectionState = enum { new, connected, failed, closed };
+pub const ConnectionState = enum { new, connecting, connected, closed };
 
 pub const InitConfig = struct {
     local_port: u16,
     remote_port: u16,
-    dtls_transport: *DtlsTransport,
 };
 
 socket: *sctp.Socket,
@@ -20,8 +19,26 @@ connection_state: ConnectionState,
 local_port: u16,
 remote_port: u16,
 dtls_transport: *DtlsTransport,
+data_channels: std.ArrayList(*DataChannel),
 
-pub fn init(sctp_transport: *SctpTranport, init_config: InitConfig) !void {
+pub fn init(init_config: InitConfig) SctpTranport {
+    return .{
+        .connection_state = .new,
+        .local_port = init_config.local_port,
+        .remote_port = init_config.remote_port,
+        .data_channels = .empty,
+        .dtls_transport = undefined,
+        .socket = undefined,
+    };
+}
+
+pub fn connect(sctp_transport: *SctpTranport, dtls_transport: *DtlsTransport) !void {
+    if (sctp_transport.connection_state != .new) return;
+
+    std.debug.print("Connect\n", .{});
+    sctp_transport.dtls_transport = dtls_transport;
+    sctp_transport.connection_state = .connecting;
+
     sctp_transport.socket = try sctp.Socket.create(.{
         .receive_cb = receive_cb,
         .ctx = sctp_transport,
@@ -34,27 +51,33 @@ pub fn init(sctp_transport: *SctpTranport, init_config: InitConfig) !void {
     try sctp_transport.socket.setInitMessage(&init_msg);
     try sctp_transport.socket.subscribe(&[_]sctp.EventType{ .assoc_change, .send_failed, .shutdown, .stream_reset });
 
-    sctp_transport.connection_state = .new;
-    sctp_transport.dtls_transport = init_config.dtls_transport;
-    sctp_transport.local_port = init_config.local_port;
-    sctp_transport.remote_port = init_config.remote_port;
-}
-
-pub fn connect(sctp_transport: *SctpTranport) !void {
     const socket = sctp_transport.socket;
     sctp.registerAddress(sctp_transport);
     errdefer sctp.deregisterAddress(sctp_transport);
 
-    var sock_addr = sctp.SockaddrConn.init(std.mem.nativeToBig(u16, sctp_transport.local_port), null);
+    var sock_addr = sctp.SockaddrConn.init(std.mem.nativeToBig(u16, sctp_transport.local_port), sctp_transport);
     try socket.bind(&sock_addr);
 
     var remote_addr = sctp.SockaddrConn.init(std.mem.nativeToBig(u16, sctp_transport.remote_port), sctp_transport);
     try socket.connect(&remote_addr);
 }
 
-pub fn deinit(sctp_transport: *SctpTranport) void {
-    sctp.deregisterAddress(sctp_transport);
+pub fn close(sctp_transport: *SctpTranport) void {
+    if (sctp_transport.connection_state == .closed) return;
+    sctp_transport.connection_state = .closed;
     sctp_transport.socket.close();
+}
+
+pub fn deinit(sctp_transport: *SctpTranport, allocator: std.mem.Allocator) void {
+    for (sctp_transport.data_channels.items) |data_channel| {
+        allocator.destroy(data_channel);
+    }
+    sctp_transport.data_channels.deinit(allocator);
+    sctp.deregisterAddress(sctp_transport);
+    switch (sctp_transport.connection_state) {
+        .connecting, .connected => sctp_transport.socket.close(),
+        else => {},
+    }
 }
 
 pub fn sendData(sctp_transport: *SctpTranport, data: []const u8) !void {
@@ -62,8 +85,30 @@ pub fn sendData(sctp_transport: *SctpTranport, data: []const u8) !void {
 }
 
 pub fn handleIncomingData(sctp_transport: *SctpTranport, data: []const u8) void {
+    switch (sctp_transport.connection_state) {
+        .new, .closed => return,
+        else => {},
+    }
+
     Logger.debug("received sctp data of length: {}", .{data.len});
     sctp.connInput(sctp_transport, data);
+}
+
+pub fn addDataChannel(sctp_transport: *SctpTranport, allocator: std.mem.Allocator, label: []const u8) !*DataChannel {
+    const data_channel = try allocator.create(DataChannel);
+    errdefer allocator.destroy(data_channel);
+    data_channel.* = .{
+        .id = @intCast(sctp_transport.data_channels.items.len + 2),
+        .label = label,
+        .ordered = true,
+    };
+    try sctp_transport.data_channels.append(allocator, data_channel);
+
+    return data_channel;
+}
+
+pub fn hasDataChannels(sctp_transport: *SctpTranport) bool {
+    return sctp_transport.data_channels.items.len > 0;
 }
 
 fn receive_cb(
